b5c71340fa85323d6abacbd2afce07b4cb52a67c
Richard O'Neil <richard.oneil@garvin-allen.com>
2015-04-20 00:29:18 -0300

    Fix namespace problems in original commit

--- ApiPractice/Areas/HelpPage/App_Start/HelpPageConfig.cs
+++ ApiPractice/Areas/HelpPage/App_Start/HelpPageConfig.cs
@@ -26,7 +26,7 @@ namespace ApiPractice.Areas.HelpPage
     public static class HelpPageConfig
     {
         [SuppressMessage("Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters",
-            MessageId = "AuthService.Areas.HelpPage.TextSample.#ctor(System.String)",
+            MessageId = "ApiPractice.Areas.HelpPage.TextSample.#ctor(System.String)",
             Justification = "End users may choose to merge this string with existing localized resources.")]
         [SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly",
             MessageId = "bsonspec",
